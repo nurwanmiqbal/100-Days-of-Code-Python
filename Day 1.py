a464fa847071ff416e3aff7820d0789a87f52c85
nurwanmiqbal <158445153+nurwanmiqbal@users.noreply.github.com>
2025-06-20 14:21:25 +0700

    Push Day 1

---
+++ Day 1.py
@@ -0,0 +1,9 @@
+#Create a greeting for the program
+print("Welcome to the Band Name Generator.")
+
+#ask the city
+city = input("Which city did you grow up in?\n")
+
+#ask the pet
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
