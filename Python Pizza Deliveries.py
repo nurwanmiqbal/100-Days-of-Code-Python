c7f9bcf40bd6738321b1a62cebcb8b758acc5c71
nurwanmiqbal <158445153+nurwanmiqbal@users.noreply.github.com>
2025-06-21 20:05:31 +0700

    Push Python Pizza

---
+++ Python Pizza Deliveries.py
@@ -0,0 +1,25 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L: ")
+pepperoni = input("Do you want pepperoni in your pizza? Y or N:")
+extra_cheese = input("Do you want extra cheese? Y or N:")
+bill = 0 
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
+# End of the code snippet
+# This code calculates the total bill for a pizza order based on size, pepperoni, and extra cheese options.
